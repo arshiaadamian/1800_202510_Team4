7d6141651d060b2cd5d85a07c2b740d2bc1f62c7
Just-Iced <plunkettjustice@gmail.com>
2025-02-19 12:19:40 -0800

    Finally, it works

--- scripts/loginSignup.js
+++ scripts/loginSignup.js
@@ -1,16 +1,54 @@
 const loginForm = document.querySelector("#log-in-ui");
 
 loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
   // Get the user's info
   const email = loginForm['emailInput'].value;
   const password = loginForm['passwordInput'].value;
+  const username = "Testing...";
+
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    
+    console.log("Signed in", cred);
+  }).catch((err) => {
+    if (err.message == "There is no user record corresponding to this identifier. The user may have been deleted.") {
+      // Otherwise, sign up the user
+      auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log("Signed up", cred);
+        db.collection("users").doc(cred.user.uid).set({
+          username: username,
+          games: [],
+        });
+      });
+    }
+  }).then((cred) => {
+    document.querySelector('#log-in-ui').submit();
+  });
+
+  // let docRef = db.collection("users").doc(email);
+  // docRef.get().then((doc) => {
+  //   if (doc.exists) {
+  //     // Sign in the user if they exist
+  //     auth.signInWithEmailAndPassword(email, password).then((cred) => {
+  //       cred.user.uid;
+  //       console.log("Signed in", cred);
+  //     });
+  //   } else {
+  //     // Otherwise, sign up the user
+  //     auth.createUserWithEmailAndPassword(email, password).then(cred => {
+  //       console.log("Signed up", cred);
+  //       db.collection("users").doc(email).set({
+  //         username: username,
+  //         games: [],
+  //       });
+  //     });
+  //   }
+  //   //document.querySelector('#log-in-ui').submit();
+  // });
 
 
-    // Sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-      console.log(cred);
-    });
-  
+
+
 
 });
 // code: "auth/email-already-in-use", message: "The email address is already in use by another account."
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,23 @@
-// function getNameFromAuth() {
-//     auth.onAuthStateChanged(user => {
-//         // Checks if a user is signed in
-//         if (user) {
-//             // Do something for the currently logged-in user here
-//             console.log(user.uid);
-//             console.log(user.displayName)
-//             userName = user.displayName;
-
-//             $('#name-goes-here').text(userName);
-//         } else {
-//             // No user is signed in
-//             console.log("No user is signed in.");
-//         }
-//     });
-// }
-// getNameFromAuth();
+function getNameFromAuth() {
+    auth.onAuthStateChanged(user => {
+        // Checks if a user is signed in
+        if (user) {
+            // Do something for the currently logged-in user here
+            console.log(user.uid);
+            console.log(user.displayName)
+            let docRef = db.collection("users").doc(user.uid);
+            let userName;
+            docRef.get().then((doc => {
+                userName = doc.data().username;
+                console.log(userName);
+                document.getElementById('name-goes-here').innerText = userName;
+            }))
+            
+            
+        } else {
+            // No user is signed in
+            console.log("No user is signed in.");
+        }
+    });
+}
+getNameFromAuth();
