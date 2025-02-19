d6eff6e4e8826bb276e8a58e38b268d3dad695e7
DeuceyMan789 <plunkettjustice@gmail.com>
2025-02-18 22:51:40 -0800

    Starting to make logging in more customizable

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
-function getNameFromAuth() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Checks if a user is signed in
-        if (user) {
-            // Do something for the currently logged-in user here
-            console.log(user.uid);
-            console.log(user.displayName)
-            userName = user.displayName;
+// function getNameFromAuth() {
+//     auth.onAuthStateChanged(user => {
+//         // Checks if a user is signed in
+//         if (user) {
+//             // Do something for the currently logged-in user here
+//             console.log(user.uid);
+//             console.log(user.displayName)
+//             userName = user.displayName;
 
-            $('#name-goes-here').text(userName);
-        } else {
-            // No user is signed in
-            console.log("No user is signed in.");
-        }
-    });
-}
-getNameFromAuth();
+//             $('#name-goes-here').text(userName);
+//         } else {
+//             // No user is signed in
+//             console.log("No user is signed in.");
+//         }
+//     });
+// }
+// getNameFromAuth();
