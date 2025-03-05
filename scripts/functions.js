7979fc280f81d567d68fade0adcfb0fedba500d5
Just-Iced <plunkettjustice@gmail.com>
2025-03-05 13:30:20 -0800

    added user profile pictures

--- scripts/functions.js
+++ scripts/functions.js
@@ -16,3 +16,34 @@ function sendMessage(to_uid, from_uid, message) {
         });
     });
 }
+const storageRef = storage.ref();
+function getUserPicture() {
+    auth.onAuthStateChanged(user => {
+        // Checks if a user is signed in
+        if (user) {
+            // Display the user's name
+            console.log(user.uid);
+            console.log(user.displayname);
+
+            let img = document.getElementById("pfp");
+            storageRef.child(`pfps/${user.uid}.png`).getDownloadURL()
+                .then((url) => {
+                    img.src = url;
+                    console.log("Tried to set the user's pfp");
+                }).catch((err) => {
+                    if (err.code === 'storage/object-not-found') {
+                        storageRef.child(`pfps/default.png`).getDownloadURL()
+                            .then((url) => {
+                                img.src = url;
+                            });
+                }});
+        } else {
+            // No user is signed in, go to the login page
+            console.log("No user is signed in.");
+            window.location.href = '/text/login.html';
+        }
+    });
+}
+
+getUserPicture();
+
---
+++ scripts/profile.js
@@ -0,0 +1,56 @@
+var fileInput = document.getElementById("pfp-input");
+var pfpDisplay = document.getElementById("pfp");
+
+
+function b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+}
+
+fileInput.addEventListener("change", event => {
+    auth.onAuthStateChanged(user => {
+        // Checks if a user is signed in
+        if (user) {
+            let pfp = fileInput.files[0];
+
+            var reader = new FileReader();
+            reader.addEventListener('load', (e) => {
+                let pfpRef = storage.ref().child(`/pfps/${user.uid}.png`);
+                console.log(e.target.result);
+                let data = e.target.result;
+                data = data.split(";")[1].split(",")[1];
+
+                let blob = b64toBlob(data, "img/png")
+                
+                pfpRef.put(blob).then((picture) => {
+                    console.log("Saving users picture");
+
+                });
+            });
+            reader.readAsDataURL(pfp);
+
+            //location.reload();
+
+        }
+    });
+});
+
