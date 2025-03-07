c73e93218ee07f861f97145b7ff3b869c304c7f6
Just-Iced <plunkettjustice@gmail.com>
2025-03-07 11:36:01 -0800

    Started allowing users to search other users for messaging

--- scripts/messages.js
+++ scripts/messages.js
@@ -16,3 +16,28 @@ function sendMessage(from_uid, to_uid, message) {
         });
     });
 }
+
+const searchButton = document.getElementById("searchButton");
+const searchTxt = document.getElementById("searchTxt");
+const cardTemplate = document.getElementById("user-card");
+const cardLocation = document.getElementById("user-cards");
+searchButton.addEventListener("click", (event) => {
+    cardLocation.innerHTML = "";
+    let userCard = cardTemplate.content.cloneNode(true);
+    let users = db.collection("users");
+    let query = users.where("username", "==", searchTxt.value);
+    query.get().then((result) => {
+        result.forEach((user) => {
+            let userData = user.data();
+            getUserPicture(user.id).then((img) => {
+                userCard.querySelector(".pfp").src = img;
+                userCard.querySelector("#username-goes-here").innerHTML = userData.username;
+                userCard.querySelector("#description-goes-here").innerHTML = userData.description;
+                cardLocation.appendChild(userCard);
+            });
+        });
+        searchTxt.value = "";
+    });
+
+});
+console.log("Loaded messages");
