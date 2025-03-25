bb5653bf1d172f6989a7fc4e69189ba90ac3ea7d
Arshia Adamian <arshiaadamian@gmail.com>
2025-03-25 15:06:07 -0700

    commited

--- scripts/search.js
+++ scripts/search.js
@@ -26,20 +26,39 @@ search.addEventListener("click", async function () {
     const container = document.getElementById("suggestions");
     const card = createUserCard(foundUser);
     container.appendChild(card);
+
+    const addFriend = document.querySelector(".addFriend");
+    addFriend.addEventListener("click", async function () {
+      await db
+        .collection("users")
+        .doc(user.uid)
+        .update({
+          friends: firebase.firestore.FieldValue.arrayUnion(foundUser.uid),
+        });
+
+      await db
+        .collection("users")
+        .doc(foundUser.uid)
+        .update({
+          friends: firebase.firestore.FieldValue.arrayUnion(user.uid),
+        });
+    });
   }
 });
 
 function createUserCard(user) {
-  const card = document.createElement('div');
-  card.className = 'card card-display';
-  card.style.width = '14rem';
-  
+  const card = document.createElement("div");
+  card.className = "card card-display";
+  card.style.width = "14rem";
+
   card.innerHTML = `
-    <img src="${user.img || '/images/default.png'}" class="card-img-top" alt="Profile Picture" />
+    <img src="${
+      user.img || "/images/default.png"
+    }" class="card-img-top" alt="Profile Picture" />
     <div class="card-body">
       <h5 class="card-title">${user.username}</h5>
       <p class="card-text">${user.description}</p>
-      <a href="#" class="btn btn-primary">Add Friend</a>
+      <a href="#" class="btn btn-primary addFriend">Add Friend</a>
     </div>
   `;
 
