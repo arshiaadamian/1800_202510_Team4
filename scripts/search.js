84e7a9092c243e4eb0a6be804ca08f560bdb83c7
Arshia Adamian <arshiaadamian@gmail.com>
2025-03-29 11:32:39 -0700

    Added an error message to the search file for when no user is found

--- scripts/search.js
+++ scripts/search.js
@@ -1,48 +1,31 @@
-const search = document.getElementById("searchButton");
-search.addEventListener("click", async function () {
-  const searchInputValue = document
-    .getElementById("searchInput")
-    .value;
+document.getElementById("searchButton").addEventListener("click", async () => {
+  const searchInput = document.getElementById("searchInput").value;
   const user = auth.currentUser;
+
   if (!user) {
-    console.log("user does not exist");
+    console.log("User not logged in.");
     return;
   }
+
   const container = document.getElementById("suggestions");
   container.innerHTML = "";
-  let userQuery = db.collection("users").where("username", "==", searchInputValue);
-  let foundUsers = await userQuery.get();
-  foundUsers.forEach(async foundUser => {
-    
-    let userData = foundUser.data();
-    userData.img = await getUserPicture(foundUser.id);
-    let card = createUserCard(userData);
-    container.appendChild(card);
 
-    const addFriend = document.querySelector(".addFriend");
-    addFriend.addEventListener("click", async function () {
-      await db
-        .collection("users")
-        .doc(user.uid)
-        .update({
-          friends: firebase.firestore.FieldValue.arrayUnion(foundUser.id),
-        });
-
-      await db
-        .collection("users")
-        .doc(foundUser.id)
-        .update({
-          friends: firebase.firestore.FieldValue.arrayUnion(user.uid),
-        });
-    });
-  });
-
-  if (!foundUsers) {
-    alert("no user found");
-  } else {
-    
+  const query = db.collection("users").where("username", "==", searchInput);
+  const results = await query.get();
+  if(results.empty){
+    let cardNotFound = CreateUserNotFoundCard();
+    container.appendChild(cardNotFound);
+    return;
   }
+
+  results.forEach(async (doc) => {
+    const data = doc.data();
+    data.img = await getUserPicture(doc.id);
+    const card = createUserCard(data);
+    container.appendChild(card);
+  });
 });
+
 
 function createUserCard(user) {
   const card = document.createElement("div");
@@ -58,6 +41,18 @@ function createUserCard(user) {
       <a href="#" class="btn btn-primary addFriend">Add Friend</a>
     </div>
   `;
+
+  return card;
+}
+
+function CreateUserNotFoundCard(){
+  const card = document.createElement("didv");
+  card.className = "card card-display";
+  card.style.width = "14rem";
+
+  card.innerHTML = `<div class="alert alert-danger" role="alert">
+  User not found!
+</div>`;
 
   return card;
 }
